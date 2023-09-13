9562ce97522961c533c2ae753da30148e4e37e46
jonthest23 <jsmora@uniboyaca.edu.co>
2023-09-12 20:03:50 -0500

    sufrimiento del primer dia

---
+++ laboratorio/src/components/Item.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      datos: [], // Inicializa un arreglo para almacenar los datos
+    };
+  },
+  methods: {
+    agregarNuevoDato(nuevoDato) {
+      this.datos.push(nuevoDato); // Agrega el nuevo dato al arreglo de datos
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="item">
+    <div class="item__container">
+      <div class="item__container__item">
+        <h4>Articulo</h4>
+        <h4>Cantidad</h4>
+        <h4>Valor</h4>
+        <h4>Total</h4>
+      </div>
+      <div class="item__container__item" v-for="item in datos" :key="item.id">
+        <p>{{item.articulo}}</p>
+        <p>{{item.cantidad}}</p>
+        <p>{{item.valor}}</p>
+        <p>{{item.total}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
---
+++ laboratorio/src/components/formulario.vue
@@ -0,0 +1,46 @@
+<template>
+    <form @submit.prevent="agregarDato">
+      <input v-model="articulo" type="text" placeholder="articulo" />
+      <input placeholder="cantidad" v-model="cantidad"/>
+      <input placeholder="valor" v-model = "valor"/>
+      <input placeholder="VU" v-model = "vu"/>
+      <input placeholder="total" v-model="total"/>
+
+      <button type="submit">Agregar</button>
+    </form>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    data() {
+      return {
+        nuevoDato: {
+            articulo: '',
+            cantidad: '',
+            vu: '',
+            valor: '',
+            total: '',
+        },
+        articulo: '',
+        cantidad: '',
+        valor: '',
+        vu: '',
+        total: '',
+      };
+    },
+    methods: {
+      agregarDato() {
+        if (this.nuevoDato) {
+          this.$emit('nuevo-dato', this.nuevoDato);
+          this.nuevoDato = {
+            articulo: this.articulo,
+            cantidad: this.cantidad,
+            vu: this.vu,
+            valor: this.valor,
+            total: this.total,
+          }; 
+        }
+      },
+    },
+  };
+  </script>
